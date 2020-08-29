a215ec5e61db9f620e0c5bab47048ad2ce98749a
zhao556600 <1072021676@qq.com>
2020-08-29 22:38:38 +0800

    202008301

--- Test/src/main/java/org/TestHello.java
+++ Test/src/main/java/org/TestHello.java
@@ -2,7 +2,7 @@ package org;
 
 public class TestHello {
     public static void main(String[] args) {
-        System.out.println("测试仓库+++++");
+        System.out.println("测试仓库-----");
         System.out.println("试试看看");
     }
 }
